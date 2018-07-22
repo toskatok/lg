dfd285eae32a88dc4b5b81b20f6b9816b0f4f608
Parham Alvani <parham.alvani@gmail.com>
2018-07-22 18:51:27 +0430

    Creates separate runner.

--- generators/generator.go
+++ generators/generator.go
@@ -13,22 +13,9 @@
 
 package generators
 
-import "time"
-
 // Generator generates data whenever you want
 // based on given input. input can be nil.
 type Generator interface {
 	Generate(input interface{}) ([]byte, error)
 	Topic() []byte
-}
-
-// Run given generator on given duration
-func Run(g Generator, d time.Duration) {
-	sendTick := time.Tick(d)
-
-	for {
-		select {
-		case <-sendTick:
-		}
-	}
 }
