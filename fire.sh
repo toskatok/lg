d2908c57dfa36e842abc01be519c3a527d4fc543
Parham Alvani <parham.alvani@gmail.com>
2018-08-01 18:30:30 +0430

    Better fire script.

--- fire.sh
+++ fire.sh
@@ -7,10 +7,47 @@ end() {
         pkill mqttlg
 }
 
-for i in `seq 1 100`; do
-        dev=$(($i + 10))
-        ./mqttlg --rate 10s --deveui $dev --broker 192.168.0.200:1883 &
-        echo "mqttlg $i: $!"
+usage() {
+        echo "fire set your platform on fire :D"
+        echo "$0 -b [broker] -r [rate]"
+        echo "-b [broker]: broker url e.g. 127.0.0.1:1883"
+        echo "-r [rate]: rate e.g. 10s"
+}
+
+main() {
+        local rate
+        local broker
+
+        rate=$1
+        broker=$2
+
+        for i in `seq 1 100`; do
+                dev=$(($i + 10))
+                ./mqttlg --rate $rate --deveui $dev --broker $broker &
+                if [ $? ]; then
+                        echo "mqttlg $i: $!"
+                else
+                        exit
+                fi
+                echo
+        done
+
+        cat
+}
+
+
+rate="10s"
+broker="127.0.0.1:1883"
+
+while getopts 'r:b:' argv; do
+        case $argv in
+                r)
+                        rate=$OPTARG
+                        ;;
+                b)
+                        broker=$OPTARG
+                        ;;
+        esac
 done
 
-cat
+main $rate $broker
