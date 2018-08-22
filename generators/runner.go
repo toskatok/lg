16a0fcbf5e17f6d7403d7fe43b82961aef955b99
Parham Alvani <parham.alvani@gmail.com>
2018-08-22 16:55:27 +0430

    Adds stop method and mqtt client.

--- generators/runner.go
+++ generators/runner.go
@@ -14,7 +14,9 @@
 package generators
 
 import (
+	"fmt"
 	"log"
+	"math/rand"
 	"sync"
 	"time"
 
@@ -22,63 +24,95 @@ import (
 	"github.com/yosssi/gmq/mqtt/client"
 )
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 // Source is called in order to create source data for generate
 type Source func() interface{}
 
 // Runner runs given generator in specific intervals
 type Runner struct {
-	g Generator
-	d time.Duration
-	p int64
-	s Source
+	generator Generator
+	duration  time.Duration
+	counter   int64
+	source    Source
 
 	cli *client.Client
 	lck sync.RWMutex
+
+	stop chan struct{}
 }
 
 // NewRunner creates new runner
-func NewRunner(g Generator, d time.Duration, s Source, cli *client.Client) Runner {
-	return Runner{
-		g: g,
-		d: d,
-		p: 0,
-		s: s,
+func NewRunner(g Generator, d time.Duration, s Source, broker string) (Runner, error) {
+	var r = Runner{
+		generator: g,
+		duration:  d,
+		counter:   0,
+		source:    s,
 
-		cli: cli,
+		stop: make(chan struct{}),
 	}
+
+	// Create an MQTT Client.
+	r.cli = client.New(&client.Options{
+		// Define the processing of the error handler.
+		ErrorHandler: func(err error) {
+			log.Println(err)
+		},
+	})
+
+	// Connect to the MQTT Server.
+	if err := r.cli.Connect(&client.ConnectOptions{
+		Network:  "tcp",
+		Address:  broker,
+		ClientID: []byte(fmt.Sprintf("I1820-lg-%d", rand.Intn(1024))),
+	}); err != nil {
+		return r, err
+	}
+
+	return r, nil
 }
 
 // Count returns number of generated messages
 func (r *Runner) Count() int64 {
 	r.lck.RLock()
 	defer r.lck.RUnlock()
-	return r.p
+	return r.counter
+}
+
+// Stop stops the runner
+func (r *Runner) Stop() {
+	close(r.stop)
 }
 
 // Run runs the runner :joy:
 func (r *Runner) Run() {
-	sendTick := time.Tick(r.d)
+	sendTick := time.Tick(r.duration)
 
 	go func() {
 		for {
 			select {
 			case <-sendTick:
-				message, err := r.g.Generate(r.s())
+				message, err := r.generator.Generate(r.source())
 				if err != nil {
 					log.Printf("Generator Generate: %s", err)
 				}
 
 				if err := r.cli.Publish(&client.PublishOptions{
 					QoS:       mqtt.QoS0,
-					TopicName: r.g.Topic(),
+					TopicName: r.generator.Topic(),
 					Message:   message,
 				}); err != nil {
 					log.Printf("MQTT Publish: %s", err)
 				}
 
 				r.lck.Lock()
-				r.p++
+				r.counter++
 				r.lck.Unlock()
+			case <-r.stop:
+				return
 			}
 		}
 	}()
