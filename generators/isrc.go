0ede350a3be1dbbd8b712c54213a0773f99a8d00
Parham Alvani <parham.alvani@gmail.com>
2018-10-12 00:32:22 +0330

    Removes need of lora.

--- generators/isrc.go
+++ generators/isrc.go
@@ -20,12 +20,42 @@ import (
 	"time"
 
 	"github.com/2tvenom/cbor"
-	"github.com/I1820/link/protocols/lora"
 )
 
+// RxMessage contains payloads received from your nodes in loraserver.io
+type RxMessage struct {
+	ApplicationID   string
+	ApplicationName string
+	DeviceName      string
+	DevEUI          string
+	FPort           int
+	FCnt            int
+	RxInfo          []RxInfo
+	TxInfo          TxInfo
+	Data            []byte
+}
+
+// RxInfo contains reception infomation of a lara gateway that
+// payload is received from it.
+type RxInfo struct {
+	Mac     string
+	Name    string
+	Time    time.Time
+	RSSI    int     `json:"rssi"`
+	LoRaSNR float64 `json:"LoRaSNR"`
+}
+
+// TxInfo contains transmission information of a lora gateway that
+// payload is received from it.
+type TxInfo struct {
+	Frequency int
+	Adr       bool
+	CodeRate  string
+}
+
 // ISRCGenerator generates data based on
-// [lora structure](https://github.com/aiotrc/uplink/blob/master/lora/messages.go) protocol
-// and [cbor](http://cbor.io/) model.
+// RxMessage structure as is described above and then encode it
+// with [cbor](http://cbor.io/).
 // for historical reasons for refer to it as ISRC protocol
 type ISRCGenerator struct {
 	ApplicationID   string `mapstructure:"applicationID"`
@@ -51,15 +81,15 @@ func (g ISRCGenerator) Generate(input interface{}) ([]byte, error) {
 	}
 
 	// lora message
-	message, err := json.Marshal(lora.RxMessage{
+	message, err := json.Marshal(RxMessage{
 		ApplicationID:   g.ApplicationID,
 		ApplicationName: g.ApplicationName,
 		DeviceName:      g.DeviceName,
 		DevEUI:          g.DevEUI,
 		FPort:           5,
 		FCnt:            10,
-		RxInfo: []lora.RxInfo{
-			lora.RxInfo{
+		RxInfo: []RxInfo{
+			RxInfo{
 				Mac:     g.GatewayMac,
 				Name:    fmt.Sprintf("gateway-%s", g.GatewayMac),
 				Time:    time.Now(),
@@ -67,7 +97,7 @@ func (g ISRCGenerator) Generate(input interface{}) ([]byte, error) {
 				LoRaSNR: 10,
 			},
 		},
-		TxInfo: lora.TxInfo{
+		TxInfo: TxInfo{
 			Frequency: 868100000,
 			Adr:       true,
 			CodeRate:  "4/6",
