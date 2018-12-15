d5fa623eb793a898606711c81de644354d25054c
Parham Alvani <parham.alvani@gmail.com>
2018-12-16 02:16:34 +0330

    Instance model and its creator are ready.

---
+++ models/instance.go
@@ -0,0 +1,139 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 16-12-2018
+ * |
+ * | File Name:     instance.go
+ * +===============================================
+ */
+
+package models
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"text/template"
+	"time"
+
+	"github.com/I1820/lg/core"
+	"github.com/I1820/lg/generators"
+	"github.com/mitchellh/mapstructure"
+)
+
+// Instance represents a load generator instance. Each instance has a runner with
+// a generator that generates messages.
+// R and G are public and consumers can use their public methods in their advanced use cases.
+type Instance struct {
+	R core.Runner
+	G generators.Generator
+
+	// message is used for populating the data templates.
+	message struct {
+		Count int64 // send message counter
+	}
+}
+
+// NewInstance creates new load generator instance with given configuration
+func NewInstance(config Config, rate time.Duration, destination string) (*Instance, error) {
+	instance := &Instance{}
+
+	// Create parent template with some useful function
+	tmpl := template.New("lg").Funcs(template.FuncMap{
+		"now":   time.Now,
+		"randn": rand.Intn,
+	})
+
+	// Read data from the given data templates, and will prase the template if it exists.
+	for i, d := range config.Messages {
+		for k, v := range d {
+			if s, ok := v.(string); ok { // parses all strings
+				p, err := tmpl.New(fmt.Sprintf("lg-%d-%s", i, k)).Parse(s)
+				if err != nil {
+					return nil, err
+				}
+				d[k] = p
+			}
+		}
+	}
+
+	// Generator selection and configuration
+	var g generators.Generator
+	switch config.Generator.Name {
+	case "isrc": // generators/isrc.go
+		var isrc generators.ISRCGenerator
+		// load genrator information from configuration file
+		if err := mapstructure.Decode(config.Generator.Info, &isrc); err != nil {
+			return nil, err
+		}
+		g = isrc
+	case "atrovan": // generators/atrovan.go
+		var atrovan generators.AtrovanGenerator
+		// load genrator information from configuration file
+		if err := mapstructure.Decode(config.Generator.Info, &atrovan); err != nil {
+			return nil, err
+		}
+		g = atrovan
+	case "fanco": // generators/fanco.go
+		var fanco generators.FancoGenerator
+		// load genrator information from configuration file
+		if err := mapstructure.Decode(config.Generator.Info, &fanco); err != nil {
+			return nil, err
+		}
+		g = fanco
+	case "ttn": // generators/ttn.go
+		var ttn generators.TTNGenerator
+		// load genrator information from configuration file
+		if err := mapstructure.Decode(config.Generator.Info, &ttn); err != nil {
+			return nil, err
+		}
+		g = ttn
+	default:
+		return nil, fmt.Errorf("Generator %s is not supported yet", config.Generator.Name)
+	}
+	instance.G = g
+
+	// Runner creation
+	r, err := core.NewRunner(core.RunnerConfig{
+		Generator: g,
+		Duration:  rate,
+		Pick: func() interface{} { // runs on each message
+			instance.message.Count++
+			d := make(map[string]interface{})
+			for k, v := range config.Messages[rand.Intn(len(config.Messages))] {
+				if tmpl, ok := v.(*template.Template); ok {
+					var b strings.Builder
+					if err := tmpl.Execute(&b, instance.message); err != nil {
+						continue
+					}
+					d[k] = b.String()
+				} else {
+					d[k] = v
+				}
+			}
+			return d
+		},
+		URL:   destination,
+		Token: config.Token,
+	})
+	if err != nil {
+		return nil, err
+	}
+	instance.R = r
+
+	return instance, nil
+}
+
+// Run runs the instance (please note that runners run in new go routine by default)
+func (i *Instance) Run() {
+	i.R.Run()
+}
+
+// Stop stops the instance
+func (i *Instance) Stop() {
+	i.R.Stop()
+}
