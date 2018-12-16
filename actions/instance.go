7fce04f745b74f6c0ddb58c7886d928132703f9e
Parham Alvani <parham.alvani@gmail.com>
2018-12-16 14:28:29 +0330

    Completes create action for instances resource.

--- actions/instance.go
+++ actions/instance.go
@@ -15,9 +15,14 @@ package actions
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/I1820/lg/models"
 	"github.com/gobuffalo/buffalo"
 )
+
+// list of the running instances
+var instances []models.Instance
 
 // InstancesResource manages instances of load generators.
 type InstancesResource struct {
@@ -27,4 +32,30 @@ type InstancesResource struct {
 // List returns all running instances
 func (v InstancesResource) List(c buffalo.Context) error {
 	return c.Render(http.StatusOK, r.JSON([]int{}))
+}
+
+// Create runs new generator instance. This function is mapped
+// to the path POST /instances
+func (v InstancesResource) Create(c buffalo.Context) error {
+	var config models.Config
+	if err := c.Bind(&config); err != nil {
+		return c.Error(http.StatusBadRequest, err)
+	}
+
+	rate, err := time.ParseDuration(c.Param("rate"))
+	if err != nil {
+		rate = 1 * time.Millisecond
+	}
+	destination := c.Param("destination")
+	if destination == "" {
+		destination = "mqtt://127.0.0.1:1883"
+	}
+
+	i, err := models.NewInstance(config, rate, destination)
+	if err != nil {
+		return c.Error(http.StatusInternalServerError, err)
+	}
+	i.Run()
+
+	return c.Render(http.StatusOK, r.JSON(true))
 }
