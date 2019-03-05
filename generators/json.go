6c2743d7548dab143639312c03a5f44af1096b1a
Parham Alvani <parham.alvani@gmail.com>
2019-03-05 08:40:41 +0330

    Implements pure json generator.

---
+++ generators/json.go
@@ -0,0 +1,24 @@
+package generators
+
+import (
+	"encoding/json"
+)
+
+// JSONGenerator generates data with json format
+type JSONGenerator struct {
+	Name string `mapstructure:"name"`
+}
+
+// Topic returns given name as topic
+func (g JSONGenerator) Topic() string {
+	return g.Name
+}
+
+// Generate generates data message json
+func (g JSONGenerator) Generate(input interface{}) ([]byte, error) {
+	b, err := json.Marshal(input)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
