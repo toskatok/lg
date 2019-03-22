60632be7498d9aa359f482f558c3c0ddcd2a7413
Parham Alvani <parham.alvani@gmail.com>
2019-03-22 11:37:02 +0430

    Runs echo server in main.

--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
-	"github.com/toskatok/lg/actions"
+	"github.com/I1820/wf/actions"
 )
 
 func main() {
 	fmt.Println("18.20 at Sep 07 2016 7:20 IR721")
 
-	app := actions.App()
-	if err := app.Serve(); err != nil {
-		log.Fatal(err)
+	e := actions.App()
+	go func() {
+		if err := e.Start(":8080"); err != http.ErrServerClosed {
+			log.Fatalf("API Service failed with %s", err)
+
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	fmt.Println("18.20 As always ... left me alone")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		log.Printf("API Service failed on exit: %s", err)
+
 	}
 }
