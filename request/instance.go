916d17126039ac8eba6cb29f82b8a000f445b69e
Parham Alvani <parham.alvani@gmail.com>
2020-04-22 16:51:44 +0430

    feat: Add Validation Method

--- request/instance.go
+++ request/instance.go
@@ -1,9 +1,16 @@
 package request
 
-import "github.com/toskatok/lg/instance"
+import (
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/toskatok/lg/instance"
+)
 
 // Instance creation request
 type Instance struct {
 	Name string
 	instance.Config
+}
+
+func (r Instance) Validate() error {
+	return validation.ValidateStruct(&r, validation.Field(&r.Name, validation.Required))
 }
